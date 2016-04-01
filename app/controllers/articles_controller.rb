fe6f5d39f3e055ff8deadccd143369df890a9af9
Miguel Casas <miguel@kommit.co>
2016-03-31 22:25:24 -0500

    add gem will_pagination to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 
 
   def index
-    @articles = Article.all.order('created_at DESC')
+    @articles = Article.all.order('created_at DESC').paginate(:page => params[:page],  :per_page => 1)
   end
 
   def new
